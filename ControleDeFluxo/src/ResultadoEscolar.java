9d22b091d50d3cbb46b2e9d2ef16962d492b3707
pedrold10 <pedrogabriel12423@gmail.com>
2023-08-25 09:54:12 -0300

    Utilizando operador ternário

--- ControleDeFluxo/src/ResultadoEscolar.java
+++ ControleDeFluxo/src/ResultadoEscolar.java
@@ -1,15 +1,8 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 5;
-
-        if(nota < 5)
-            System.out.println("Aluno reprovado!");
-    
-        else if(nota < 7 )
-            System.out.println("Aluno está de recuperação");
-
-        else
-            System.out.println("Aluno aprovado!");
+        Integer nota = 7;
+        String resultado = nota >=7 ? "Aluno aprovado" : "Aluno Reprovado";
+        System.out.println(resultado);
     }
     
 }
