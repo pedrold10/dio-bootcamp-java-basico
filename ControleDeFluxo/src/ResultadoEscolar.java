80fff249a736dd214776502f631bcdd4f38a1bc7
pedrold10 <pedrogabriel12423@gmail.com>
2023-08-25 09:51:17 -0300

    Sistema de nota com if e else

--- ControleDeFluxo/src/ResultadoEscolar.java
+++ ControleDeFluxo/src/ResultadoEscolar.java
@@ -1,6 +1,15 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        
+        int nota = 5;
+
+        if(nota < 5)
+            System.out.println("Aluno reprovado!");
+    
+        else if(nota < 7 )
+            System.out.println("Aluno está de recuperação");
+
+        else
+            System.out.println("Aluno aprovado!");
     }
     
 }
